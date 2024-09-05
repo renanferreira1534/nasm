0c352594331d40f6f0d9f904c364517feb4f9ce8
renan ferreira <renanferreira1534@gmail.com>
2024-09-05 14:29:47 +0000

    criação de um novo arquivo

--- hello.asm
+++ hello.asm
@@ -1,14 +1,16 @@
-global _start
-    section .text
-    _start:
+section .data
+    msg db 'hello, world',0
+
+section .text
+    global _start
+
+ _start:
     mov rax,1
-    mov rdi,1
-    mov rsi,message
-    mov rdx,11
+    mov rdi,1   
+    mov rsi,msg
+    mov rdx, 13
     syscall
+
     mov rax,60
-    xor rdi,rdi
+    xor rdi, rdi
     syscall
-    section .data
-    message:db 'hello,world',10
-    
